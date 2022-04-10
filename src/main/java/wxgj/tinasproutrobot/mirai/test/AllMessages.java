4ca2903ce991df1adc8c6b16f64a1400491d8f15
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-10 20:29:07 +0800

    添加获取群聊消息中的文件,写废了

--- src/main/java/wxgj/tinasproutrobot/mirai/test/AllMessages.java
+++ src/main/java/wxgj/tinasproutrobot/mirai/test/AllMessages.java
@@ -1,5 +1,10 @@
 package wxgj.tinasproutrobot.mirai.test;
 
+import kotlin.Unit;
+import kotlin.coroutines.Continuation;
+import kotlin.coroutines.CoroutineContext;
+import kotlinx.coroutines.flow.Flow;
+import kotlinx.coroutines.flow.FlowCollector;
 import net.mamoe.mirai.contact.file.AbsoluteFile;
 import net.mamoe.mirai.contact.file.AbsoluteFolder;
 import net.mamoe.mirai.contact.file.RemoteFiles;
@@ -7,10 +12,13 @@ import net.mamoe.mirai.event.EventHandler;
 import net.mamoe.mirai.event.SimpleListenerHost;
 import net.mamoe.mirai.event.events.FriendMessageEvent;
 import net.mamoe.mirai.event.events.GroupMessageEvent;
-import net.mamoe.mirai.message.data.FileMessage;
 import net.mamoe.mirai.utils.MiraiLogger;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 public class AllMessages extends SimpleListenerHost {
+    MiraiLogger logger;
+
     // 所有方法类型
     // T 表示任何 Event 类型.
     // void onEvent(T)
@@ -25,20 +33,48 @@ public class AllMessages extends SimpleListenerHost {
 
     @EventHandler
     private void onGroupMessage(GroupMessageEvent event) {
-        MiraiLogger logger = event.getBot().getLogger();
+        logger = event.getBot().getLogger();
 //        RemoteFiles files = event.getGroup().getFiles();
 //logger.info("RomoteFiles:"+files.toString());
 //        AbsoluteFolder root = files.getRoot();
 //        logger.debug("AbsoluteFolder:"+root.children());
-        FileMessage fileMessage = event.getMessage().get(FileMessage.Key);
-        if (fileMessage != null) {
-            AbsoluteFile remoteFiles = fileMessage.toAbsoluteFile(event.getGroup());
-            logger.info("文件名字：" + remoteFiles.getName());
-            logger.info("文件链接：" + remoteFiles.getUrl());
-            return;
+
+        if (event.getMessage().contentToString().equals("游历文件")) {
+            logger.info("草" + event.getMessage().contentToString());
+//            Iterator<RemoteFiles> iterator = event.getGroup().getFiles();
+//            List<RemoteFile> list = event.getGroup().getFilesRoot().listFilesCollection();
+            RemoteFiles remoteFiles = event.getGroup().getFiles();
+            logger.debug("RemoteFiles:" + remoteFiles.getRoot().getName());
+            AbsoluteFolder absoluteFile = remoteFiles.getRoot();
+            logger.info("AbsoluteFolder:" + absoluteFile.getName());
+            Flow<AbsoluteFile> absoluteFolders = remoteFiles.getRoot().files();
+            absoluteFolders.collect(new FlowCollector<AbsoluteFile>() {
+                @Nullable
+                @Override
+                public Object emit(AbsoluteFile absoluteFile, @NotNull Continuation<? super Unit> continuation) {
+                    logger.info("AbsoluteFile:" + absoluteFile.getName());
+                    return null;
+                }
+            },null);
+
         }
+
+//        FileMessage fileMessage = event.getMessage().get(FileMessage.Key);
+//        if (fileMessage != null) {
+//            AbsoluteFile remoteFiles = fileMessage.toAbsoluteFile(event.getGroup());
+//            logger.info("文件名字：" + remoteFiles.getName());
+//            logger.info("文件链接：" + remoteFiles.getUrl());
+//            //return;
+//        }
+
 
     }
 
+    @Override
+    public void handleException(@NotNull CoroutineContext context, @NotNull Throwable exception) {
+        // 处理事件处理时抛出的异常
+        logger.error(exception);
+
+    }
 
 }
