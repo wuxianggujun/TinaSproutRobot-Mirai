f4fc5d9a9a32e537415685406c879322528bab27
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-16 20:55:59 +0800

    我要搞权限，并且创建管理员权限

--- src/main/kotlin/TinaSproutBotPlugin.kt
+++ src/main/kotlin/TinaSproutBotPlugin.kt
@@ -5,12 +5,26 @@ import net.mamoe.mirai.console.command.Command
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.data.PluginData
+import net.mamoe.mirai.console.permission.AbstractPermitteeId
+import net.mamoe.mirai.console.permission.Permission
+import net.mamoe.mirai.console.permission.PermissionId
+import net.mamoe.mirai.console.permission.PermissionService
+import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
+import net.mamoe.mirai.console.permission.PermissionService.Companion.permit
+import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
+import net.mamoe.mirai.console.plugin.name
+import net.mamoe.mirai.contact.Member
+import net.mamoe.mirai.contact.isOperator
 import net.mamoe.mirai.event.EventChannel
 import net.mamoe.mirai.event.events.BotEvent
 import net.mamoe.mirai.event.events.BotOnlineEvent
+import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.message.data.MessageSource.Key.quote
+import net.mamoe.mirai.message.data.time
+import net.mamoe.mirai.message.nextMessage
 import wxgj.tinasproutrobot.mirai.bot.config.SettingsConfig
 import wxgj.tinasproutrobot.mirai.bot.data.TinaSproutRobotPluginData
 import wxgj.tinasproutrobot.mirai.command.AdminCommand
@@ -31,6 +45,8 @@ object TinaSproutBotPlugin : KotlinPlugin(
     private lateinit var commands: List<Command>
     private lateinit var data: List<PluginData>
 
+    private lateinit var adminPermission: Permission
+
     override fun onEnable() {
         logger.info("TinaSproutBotPlugin Loaded")
 
@@ -41,6 +57,14 @@ object TinaSproutBotPlugin : KotlinPlugin(
             it.reload()
         }
 
+        commands.forEach {
+            it.register()
+        }
+        adminPermission = PermissionService.INSTANCE.register(
+            PermissionId(name,"admin"),"Admin Permission"
+        )
+        //AbstractPermitteeId.AnyContact.permit(MasterCommand.permission)
+
         val eventChannel = this.globalEventChannel().parentScope(this)
 
         if (master == null) {
@@ -49,17 +73,49 @@ object TinaSproutBotPlugin : KotlinPlugin(
         logger.info("主人：$master")
 
         eventChannel.filterIsInstance(BotOnlineEvent::class.java)
-            .filter { (bot): BotOnlineEvent -> bot.id == SettingsConfig.roBot }
+            .filter { event: BotOnlineEvent -> event.bot.id == SettingsConfig.roBot }
             .subscribeAlways<BotOnlineEvent> {
                 val bot: Bot = this.bot
-                val eventChannel: EventChannel<BotEvent> = bot.eventChannel
-                commands.forEach {
-                    it.register()
+                val botEvent: EventChannel<BotEvent> = bot.eventChannel
+                botEvent.subscribeAlways<GroupMessageEvent> {
+                    val nextMsg = nextMessage()
+                    //判断发送时间
+                    if (nextMsg.time - time < 60) {
+                        group.sendMessage(message.quote() + "你好快啊")
+                    }
+                    if (checkPermission(sender)){
+                        logger.info("我权限比较大哈")
+                    }
+                    logger.info("草：$message")
                 }
             }
 
     }
 
+    fun checkPermission(sender: Member): Boolean {
+        when (SettingsConfig.permitMode) {
+            0 -> {
+                return true
+            }
+            1 -> {
+                if (SettingsConfig.master == sender.id)
+                    return true
+            }
+            2 -> {
+                if (SettingsConfig.master == sender.id)
+                    return true
+                return sender.isOperator()
+            }
+            3 -> {
+                return sender.permitteeId.hasPermission(adminPermission)
+
+            }
+            else -> {
+                logger.error("权限设置信息错误，请检查权限模式设置")
+            }
+        }
+        return false
+    }
 
     override fun onDisable() {
         data.forEach {
