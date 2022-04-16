bc4001a2e943dcbcfc4215baaedfac02bb8f8256
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-16 18:07:51 +0800

    随便复制粘贴了注释

--- src/main/kotlin/bot/TinaSproutRobotPluginConfig.kt
+++ src/main/kotlin/bot/TinaSproutRobotPluginConfig.kt
@@ -5,10 +5,20 @@ import net.mamoe.mirai.console.data.ReadOnlyPluginConfig
 import net.mamoe.mirai.console.data.ValueDescription
 import net.mamoe.mirai.console.data.value
 
-object TinaSproutRobotPluginConfig : AutoSavePluginConfig("TinaSproutRobotPluginConfig") {
+object TinaSproutRobotPluginConfig : ReadOnlyPluginConfig("Settings") {
     val min: Int by value(60)
     val max: Int by value(6000)
     val timer: Int by value(10)
+    @ValueDescription(
+        """
+        插件权限控制设置
+        0 为所有人都可以控制
+        1 为只有插件主人可以进行配置
+        2 为群管理员也可以配置
+        3 为拥有权限（mirai-setu:admin）者可以配置
+        """
+    )
+    val permitMode by value(1)
 
     val clearTimer: String by value("00:00")
 
