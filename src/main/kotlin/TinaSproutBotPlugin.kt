bee94d66ca4ef945558126dce1fd72f610655f0a
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-17 16:28:50 +0800

    发现没有欢迎新用户这个bug

--- src/main/kotlin/TinaSproutBotPlugin.kt
+++ src/main/kotlin/TinaSproutBotPlugin.kt
@@ -6,17 +6,24 @@ import net.mamoe.mirai.console.command.CommandManager.INSTANCE.register
 import net.mamoe.mirai.console.command.CommandManager.INSTANCE.unregister
 import net.mamoe.mirai.console.data.PluginData
 import net.mamoe.mirai.console.permission.AbstractPermitteeId
+import net.mamoe.mirai.console.permission.PermissionId
+import net.mamoe.mirai.console.permission.PermissionService
 import net.mamoe.mirai.console.permission.PermissionService.Companion.cancel
+import net.mamoe.mirai.console.permission.PermissionService.Companion.getPermittedPermissions
 import net.mamoe.mirai.console.permission.PermissionService.Companion.permit
+import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.event.EventChannel
 import net.mamoe.mirai.event.events.BotEvent
 import net.mamoe.mirai.event.events.BotOnlineEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
+import net.mamoe.mirai.event.events.MemberJoinEvent
 import net.mamoe.mirai.event.globalEventChannel
+import net.mamoe.mirai.message.data.*
 import wxgj.tinasproutrobot.mirai.bot.config.SettingsConfig
 import wxgj.tinasproutrobot.mirai.bot.data.GroupPermissionData
+import wxgj.tinasproutrobot.mirai.bot.data.GroupPermissionData.groupWelcomeMessage
 import wxgj.tinasproutrobot.mirai.bot.data.TinaSproutRobotPluginData
 import wxgj.tinasproutrobot.mirai.command.AdminCommand
 import wxgj.tinasproutrobot.mirai.command.GroupCommand
@@ -29,10 +36,7 @@ object TinaSproutBotPlugin : KotlinPlugin(
         info("提娜——斯普朗特")
     }) {
 
-    //private lateinit var autoMaticResponseMap: MutableMap<Long, Boolean>
     private var master: Long? = null
-    private var preMessage: String = ""
-    private var num: Int = 1
 
     //命令列表用于统一管理，统一注册
     private lateinit var commands: List<Command>
@@ -62,6 +66,9 @@ object TinaSproutBotPlugin : KotlinPlugin(
             logger.warning("无法自动授予权限，请自行使用权限管理来授予权限")
         }
 
+        val gwp = PermissionId("group", "welcome.message")
+        PermissionService.INSTANCE.register(gwp, "群欢迎你")
+
         val eventChannel = this.globalEventChannel().parentScope(this)
 
         if (master == null) {
@@ -74,11 +81,30 @@ object TinaSproutBotPlugin : KotlinPlugin(
             .subscribeAlways<BotOnlineEvent> {
                 val bot: Bot = this.bot
                 val botEvent: EventChannel<BotEvent> = bot.eventChannel
-                botEvent.subscribeAlways<GroupMessageEvent> {
 
-                    logger.info("草：$message")
+                botEvent.subscribeAlways<GroupMessageEvent> {
+                    val hasPerm = group.permitteeId.getPermittedPermissions().any { it.id == gwp }
+                    logger.info("为什么没有用")
+                    if (hasPerm && message.content == "#r") {
+
+                        group.sendMessage(QuoteReply(source) + Dice((1..6).random()))
+                    }
                 }
+
+                botEvent.subscribeAlways<MemberJoinEvent> {
+                    val hasPerm = group.permitteeId.getPermittedPermissions().any { it.id == gwp }
+                    if (hasPerm && groupWelcomeMessage.isNotEmpty()) {
+                        val builder = MessageChainBuilder()
+                        builder.add(At(user))
+                        builder.add("Hello Mirai :)")
+                        val msg = builder.build() // builder.asMessageChain() 也可以
+                        group.sendMessage(msg)
+
+                    }
+                }
+
             }
+
 
     }
 
@@ -126,3 +152,4 @@ object TinaSproutBotPlugin : KotlinPlugin(
 
 
 }
+
