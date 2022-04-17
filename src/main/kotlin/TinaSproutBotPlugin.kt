21220a4a2a246a206974ac6b867a73c74895daa2
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-17 10:05:50 +0800

    新增权限配置

--- src/main/kotlin/TinaSproutBotPlugin.kt
+++ src/main/kotlin/TinaSproutBotPlugin.kt
@@ -10,20 +10,17 @@ import net.mamoe.mirai.console.permission.Permission
 import net.mamoe.mirai.console.permission.PermissionId
 import net.mamoe.mirai.console.permission.PermissionService
 import net.mamoe.mirai.console.permission.PermissionService.Companion.cancel
-import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
 import net.mamoe.mirai.console.permission.PermissionService.Companion.permit
-import net.mamoe.mirai.console.permission.PermitteeId.Companion.permitteeId
 import net.mamoe.mirai.console.plugin.jvm.JvmPluginDescription
 import net.mamoe.mirai.console.plugin.jvm.KotlinPlugin
 import net.mamoe.mirai.console.plugin.name
-import net.mamoe.mirai.contact.Member
-import net.mamoe.mirai.contact.isOperator
 import net.mamoe.mirai.event.EventChannel
 import net.mamoe.mirai.event.events.BotEvent
 import net.mamoe.mirai.event.events.BotOnlineEvent
 import net.mamoe.mirai.event.events.GroupMessageEvent
 import net.mamoe.mirai.event.globalEventChannel
 import wxgj.tinasproutrobot.mirai.bot.config.SettingsConfig
+import wxgj.tinasproutrobot.mirai.bot.data.PermissionPluginData
 import wxgj.tinasproutrobot.mirai.bot.data.TinaSproutRobotPluginData
 import wxgj.tinasproutrobot.mirai.command.AdminCommand
 import wxgj.tinasproutrobot.mirai.command.MasterCommand
@@ -48,7 +45,7 @@ object TinaSproutBotPlugin : KotlinPlugin(
     override fun onEnable() {
         logger.info("TinaSproutBotPlugin Loaded")
 
-        data = listOf(SettingsConfig, TinaSproutRobotPluginData)
+        data = listOf(SettingsConfig, TinaSproutRobotPluginData, PermissionPluginData)
         commands = listOf(MasterCommand, AdminCommand)
 
         data.forEach {
@@ -61,7 +58,6 @@ object TinaSproutBotPlugin : KotlinPlugin(
         adminPermission = PermissionService.INSTANCE.register(
             PermissionId(name, "admin"), "Admin Permission"
         )
-        //AbstractPermitteeId.AnyContact.permit(MasterCommand.permission)
 // 授予权限
         try {
             AbstractPermitteeId.AnyContact.permit(AdminCommand.permission)
@@ -82,47 +78,39 @@ object TinaSproutBotPlugin : KotlinPlugin(
                 val bot: Bot = this.bot
                 val botEvent: EventChannel<BotEvent> = bot.eventChannel
                 botEvent.subscribeAlways<GroupMessageEvent> {
-//                    val nextMsg = nextMessage()
-//                    //判断发送时间
-//                    if (nextMsg.time - time < 60) {
-//                        group.sendMessage(message.quote() + "你好快啊")
-//                    }
-//                    if (checkPermission(sender)) {
-//                        logger.info("我权限比较大哈")
-//                    }
+
                     logger.info("草：$message")
                 }
             }
 
     }
 
-    fun checkPermission(sender: Member): Boolean {
-        when (SettingsConfig.permitMode) {
-            0 -> {
-                return true
-            }
-            1 -> {
-                if (SettingsConfig.master == sender.id)
-                    return true
-            }
-            2 -> {
-                if (SettingsConfig.master == sender.id)
-                    return true
-                return sender.isOperator()
-            }
-            3 -> {
-                return sender.permitteeId.hasPermission(adminPermission)
-
-            }
-            else -> {
-                logger.error("权限设置信息错误，请检查权限模式设置")
-            }
-        }
-        return false
-    }
+//    fun checkPermission(sender: Member): Boolean {
+//        when (SettingsConfig.permitMode) {
+//            0 -> {
+//                return true
+//            }
+//            1 -> {
+//                if (SettingsConfig.master == sender.id)
+//                    return true
+//            }
+//            2 -> {
+//                if (SettingsConfig.master == sender.id)
+//                    return true
+//                return sender.isOperator()
+//            }
+//            3 -> {
+//                return sender.permitteeId.hasPermission(adminPermission)
+//
+//            }
+//            else -> {
+//                logger.error("权限设置信息错误，请检查权限模式设置")
+//            }
+//        }
+//        return false
+//    }
 
     override fun onDisable() {
-
         // 撤销权限
         try {
             AbstractPermitteeId.AnyContact.cancel(AdminCommand.permission, true)
---
+++ src/main/kotlin/bot/data/PermissionPluginData.kt
@@ -0,0 +1,16 @@
+package wxgj.tinasproutrobot.mirai.bot.data
+
+import net.mamoe.mirai.console.data.AutoSavePluginData
+import net.mamoe.mirai.console.data.ValueDescription
+import net.mamoe.mirai.console.data.value
+
+object PermissionPluginData : AutoSavePluginData("PermissionData") {
+    @ValueDescription("组")
+    val group: MutableMap<String, MutableList<String>> by value(mutableMapOf())
+
+    @ValueDescription("组权限")
+    val groupPerm: MutableMap<String, MutableList<String>> by value(mutableMapOf())
+
+    @ValueDescription("群欢迎消息")
+    val groupWelcomeMessage: MutableMap<String, String> by value(mutableMapOf())
+}
