529e1b5b1b48d5e742852137b717164abfb0e79b
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-21 22:06:09 +0800

    直接复制了源码里面的help命令

--- src/main/kotlin/command/HelpCommand.kt
+++ src/main/kotlin/command/HelpCommand.kt
@@ -2,24 +2,25 @@ package wxgj.tinasproutrobot.mirai.command
 
 import net.mamoe.mirai.console.command.CommandManager
 import net.mamoe.mirai.console.command.CommandSender
-import net.mamoe.mirai.console.command.ConsoleCommandOwner
 import net.mamoe.mirai.console.command.SimpleCommand
-import net.mamoe.mirai.console.permission.PermissionId
 import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
-import net.mamoe.mirai.console.permission.PermitteeId
 import wxgj.tinasproutrobot.mirai.TinaSproutBotPlugin
 
-object HelpCommand : SimpleCommand(TinaSproutBotPlugin, "help", secondaryNames = arrayOf("帮助"),description = "查看指令帮助") {
+object HelpCommand :
+    SimpleCommand(TinaSproutBotPlugin, "help", secondaryNames = arrayOf("帮助"), description = "查看指令帮助") {
 
     @Handler
     suspend fun CommandSender.handle() {
-          TinaSproutBotPlugin.logger.info("我就是帮助")
-        CommandManager.getRegisteredCommands(TinaSproutBotPlugin).filter {
+        TinaSproutBotPlugin.logger.info("我就是帮助")
+        CommandManager.getRegisteredCommands(TinaSproutBotPlugin).asSequence().filter {
             permitteeId.hasPermission(it.permission)
-        }.map {
-            sendMessage(it.primaryName)
-        }
+        }.joinToString("\n\n") { commands ->
+            val lines = commands.usage.lines()
+            if (lines.isEmpty()) "/${commands.primaryName} ${commands.description}"
+            else
+                "◆ " + lines.first() + "\n" + lines.drop(1).joinToString("\n") { "  $it" }
+        }.lines().filterNot(String::isBlank).joinToString("\n")
     }
-    // for https://github.com/mamoe/mirai-console/issues/416
+
 
 }
