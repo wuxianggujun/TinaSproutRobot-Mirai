a8b85ce83832b31abddd10dae9f9ce1b77d0b635
无相孤君 <39469560+wuxianggujun@users.noreply.github.com>
2022-04-21 21:45:36 +0800

    删除一些help命令中多余的方法，修饰符

--- src/main/kotlin/command/HelpCommand.kt
+++ src/main/kotlin/command/HelpCommand.kt
@@ -1,17 +1,24 @@
 package wxgj.tinasproutrobot.mirai.command
 
+import net.mamoe.mirai.console.command.CommandManager
 import net.mamoe.mirai.console.command.CommandSender
 import net.mamoe.mirai.console.command.ConsoleCommandOwner
 import net.mamoe.mirai.console.command.SimpleCommand
+import net.mamoe.mirai.console.permission.PermissionId
+import net.mamoe.mirai.console.permission.PermissionService.Companion.hasPermission
 import net.mamoe.mirai.console.permission.PermitteeId
 import wxgj.tinasproutrobot.mirai.TinaSproutBotPlugin
 
-object HelpCommand : SimpleCommand(TinaSproutBotPlugin, "help", description = "查看指令帮助") {
+object HelpCommand : SimpleCommand(TinaSproutBotPlugin, "help", secondaryNames = arrayOf("帮助"),description = "查看指令帮助") {
 
-    @Override
     @Handler
-    public suspend fun CommandSender.handle(override: Override) {
+    suspend fun CommandSender.handle() {
           TinaSproutBotPlugin.logger.info("我就是帮助")
+        CommandManager.getRegisteredCommands(TinaSproutBotPlugin).filter {
+            permitteeId.hasPermission(it.permission)
+        }.map {
+            sendMessage(it.primaryName)
+        }
     }
     // for https://github.com/mamoe/mirai-console/issues/416
 
